ab1be5e6641e23eda17f5b7516bab51174660ea4
jonskaarupjakobsen@gmail.com <jonskaarupjakobsen@gmail.com>
2023-05-17 19:24:50 +0200

    Opdateret version history

--- Constants/VersionHistory.cs
+++ Constants/VersionHistory.cs
@@ -7,6 +7,7 @@
         /// Hvilket version bliver der kørt
         /// </summary>
         //public static string No = "V1.1 - 2022-05-15 - Initial version"; 
-        public static string No = "V1.2 - 2022-05-15 - With version control"; 
+        //public static string No = "V1.2 - 2022-05-15 - With version control"; 
+        public static string No = "V1.3 - 2022-05-17 - With latest UI changes";
     }
 }
